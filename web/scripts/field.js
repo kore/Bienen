37473abfcfdb0bad66c0d4b159822f9db573a377
Kore Nordmann <mail@kore-nordmann.de>
2015-05-15 23:19:18 +0200

    Draw target and blocks into level

--- web/scripts/field.js
+++ web/scripts/field.js
@@ -31,28 +31,56 @@ Field.prototype.loadLevel = function(level) {
                 case "t":
                 case "x":
                     map[i][j] = row[j];
+                    break;
+
+                default:
+                    map[i][j] = 0;
             }
 
             width = Math.max(width, row.length);
         };
         height = i;
     });
+    console.log(map);
     
-    var offsetX = Math.floor((this.canvas.width - (width * 50)) / 2),
-        offsetY = Math.floor((this.canvas.height - (height * 50)) / 2);
+    var offset = [
+            Math.floor((this.canvas.width - (width * 50)) / 2),
+            Math.floor((this.canvas.height - (height * 50)) / 2)
+        ];
 
     this.canvas.clear();
     for (var i = 0; i <= width; ++i) {
-        var position = offsetX + (50 * i);
-        this.canvas.path("M" + position + " " + offsetY + "L" + position + " " + (offsetY + 50 * height))
+        var position = offset[0] + (50 * i);
+        this.canvas.path("M" + position + " " + offset[1] + "L" + position + " " + (offset[1] + 50 * height))
             .attr("stroke", "#000")
             .attr("stroke-width", "2px");
     }
 
     for (var i = 0; i <= height; ++i) {
-        var position = offsetY + (50 * i);
-        this.canvas.path("M" + offsetX + " " + position + "L" + (offsetX + 50 * width) + " " + position)
+        var position = offset[1] + (50 * i);
+        this.canvas.path("M" + offset[0] + " " + position + "L" + (offset[0] + 50 * width) + " " + position)
             .attr("stroke", "#000")
             .attr("stroke-width", "2px");
+    }
+
+    for (var i = 0; i < width; ++i) {
+        for (var j = 0; j < height; ++j) {
+            var center = [
+                    offset[0] + (i * 50) + 25,
+                    offset[1] + (j * 50) + 25
+                ];
+
+            switch (map[j][i]) {
+                case "x":
+                    this.canvas.rect(center[0] - 20, center[1] - 20, 40, 40, 5)
+                        .attr("fill", "#000000");
+                    break;
+
+                case "t":
+                    this.canvas.circle(center[0], center[1], 20)
+                        .attr("fill", "#ff0000");
+                    break;
+            };
+        }
     }
 };
