a7c6db18366dac665c19f6730a3b4ed1f599b2e6
Kore Nordmann <mail@kore-nordmann.de>
2015-05-16 15:15:48 +0200

    Added some more levels

--- web/scripts/level.js
+++ web/scripts/level.js
@@ -10,4 +10,43 @@ var level = [
             "    x   \n" +
             "x     x \n"
     },
+    {
+        name: "Hindernis",
+        start: [2, 2],
+        direction: 90,
+        field:
+            "   x    \n" +
+            " x    x \n" +
+            "    xt  \n" +
+            "    x   \n" +
+            "x     x \n"
+    },
+    {
+        name: "Spirale",
+        start: [0, 0],
+        direction: 180,
+        field:
+            " x          \n" +
+            " x xxxxxxxx \n" +
+            " x x      x \n" +
+            " x x   tx x \n" +
+            " x xxxxxx x \n" +
+            " x        x \n" +
+            " xxxxxxxxxx \n" +
+            "            \n"
+    },
+    {
+        name: "Backtracking",
+        start: [0, 0],
+        direction: 180,
+        field:
+            " x   x      \n" +
+            " x x x xxxx \n" +
+            " x x x x    \n" +
+            " x x x x xxx\n" +
+            " x x   x    \n" +
+            " x xxxxxxxx \n" +
+            " x xt x   x \n" +
+            "   x    x   \n"
+    }
 ];
